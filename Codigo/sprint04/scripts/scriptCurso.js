562f4cafd1be9b9aa13d95c202cfd0ff6de8bbea
fernandamgomes <fernanda.gomes.1117337@sga.pucminas.br>
2021-12-08 13:09:52 -0300

    Update scriptCurso.js

--- Codigo/sprint04/scripts/scriptCurso.js
+++ Codigo/sprint04/scripts/scriptCurso.js
@@ -10,25 +10,26 @@ function getUrlVars() {
 
 
 function carregaNomeCurso() {
-    let tituloCurso = document.getElementById ('nomeCurso')
+    let tituloCurso = document.getElementById('nomeCurso')
     let idCurso = getUrlVars()["idCurso"]
     let nome_curso = db.cursos[idCurso].nomeCurso
     let htmlStr = ''
 
     htmlStr += `<h1 id="nomeCurso">${nome_curso}</h1>`
-    
+
 
     tituloCurso.innerHTML = htmlStr
 }
 
+
 function carregaNomeDisciplina() {
-    let tituloDisciplina = document.getElementById ('nomeDisciplina')
-    let btnVoltar = document.getElementById ('btnVoltar')
+    let tituloDisciplina = document.getElementById('nomeDisciplina')
+    let btnVoltar = document.getElementById('btnVoltar')
     let idCurso = getUrlVars()["idCurso"]
     let idDisciplina = db.cursos[idCurso].idDisciplina
-            
-    for(i=0; i < db.disciplinas.length; i++){
-        if(idDisciplina == db.disciplinas[i].id){
+
+    for (i = 0; i < db.disciplinas.length; i++) {
+        if (idDisciplina == db.disciplinas[i].id) {
             let nomeDisciplina = db.disciplinas[i].titulo
             let htmlStr = ''
 
@@ -37,12 +38,12 @@ function carregaNomeDisciplina() {
             tituloDisciplina.innerHTML = htmlStr
         }
     }
-    for(i=0; i < db.disciplinas.length; i++){
-        if(idDisciplina == db.disciplinas[i].id){
+    for (i = 0; i < db.disciplinas.length; i++) {
+        if (idDisciplina == db.disciplinas[i].id) {
             let nomeDisciplina = db.disciplinas[i].titulo
             let htmlStr = ''
 
-            htmlStr += `<a class="btn" href="disciplina.html?id=${db.disciplinas[i].id}">voltar para a ${nomeDisciplina}</a>`
+            htmlStr += `<a class="btn voltar" href="disciplina.html?id=${db.disciplinas[i].id}">voltar para ${nomeDisciplina}</a>`
 
             btnVoltar.innerHTML = htmlStr
         }
@@ -56,33 +57,32 @@ function carregaConteudoCurso() {
     let id = getUrlVars()["id"]
     let video = db.cursos[idCurso].videos[id]
     let htmlStr = ''
-
-            
-            htmlStr += 
-            `<div class="container-curso-video">
-                <iframe src="${video.urlVideo}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>            
+    htmlStr +=
+        `<div class="container-curso-video">
+                <iframe src="${video.urlVideo}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <div class="container-curso-texto">
                 <h5>${video.nomeVideo}</h5>
                 <p><span>Descrição: </span>${video.descricaoVideo}</p>
           </div>`
-        
-    
+
     containerConteudo.innerHTML = htmlStr
 }
 
-function carregaMenuConteudo () {
+
+function carregaMenuConteudo() {
     let menuConteudo = document.getElementById('menuConteudo')
     let id = getUrlVars()["idCurso"]
     let videos = db.cursos[id].videos
     let htmlStr = ''
 
-    for (i=0; i<videos.length; i++) {
+    for (i = 0; i < videos.length; i++) {
         let video = videos[i]
         htmlStr += `<li><a href="curso.html?idCurso=${id}&id=${video.idVideo}" class="btn">${video.nomeVideo}</a></li>`
     }
     menuConteudo.innerHTML = htmlStr
 }
+
 
 function carregaTitle() {
     let idCurso = getUrlVars()["idCurso"]
