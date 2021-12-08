4b69293ff1e71f6d63d837272f3adc3ca24246ea
fernandamgomes <fernanda.gomes.1117337@sga.pucminas.br>
2021-12-08 12:57:56 -0300

    add funcionalidade salvar curso

---
+++ Codigo/sprint04/scripts/salvarCurso.js
@@ -0,0 +1,91 @@
+var dbCursosSalvos = {
+    salvos: [],
+};
+
+
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+        function (m, key, value) {
+            vars[key] = value;
+        });
+
+    return vars;
+}
+
+function salvarCurso () {
+    let idCurso = getUrlVars()["idCurso"];
+    let teste = 0;  
+    let tmp = {};
+    let dataSalvos;
+
+    // checar se usuario ta logado
+    //if (!usuarioCorrente.login) {
+        //window.location.href = LOGIN_URL;
+    //}
+    //else {
+        dataSalvos = JSON.parse(localStorage.getItem("cursosSalvos"))
+        if (!dataSalvos) {
+            dataSalvos = dbCursosSalvos.salvos
+        }
+        for (i = 0; i < dataSalvos.length; i++) {
+            if (dataSalvos[i].idCurso == idCurso) {
+                teste = 1;
+            }
+        }
+        if (teste == 0) {
+            alert("Curso salvado com sucesso")
+            let curso = db.cursos[idCurso]
+            tmp = {
+                "idDisciplina": curso.idDisciplina, 
+                "idCurso": idCurso,
+                "nomeCurso": curso.nomeCurso,
+                "descricao": curso.descricao,
+                "img": curso.img,
+            }
+            dataSalvos.push(tmp);
+        }
+        else {
+            alert("Você já salvou esse curso anteriormente")
+        }
+
+        localStorage.setItem("cursosSalvos", JSON.stringify(dataSalvos))
+    //}
+}
+
+
+// carrega a lista de cursos salvos
+function carregaCursosSalvos () {
+    let containerCurso = document.getElementById('lista-cursos-salvos')
+    let htmlStr = ''
+    dataSalvos = JSON.parse(localStorage.getItem("cursosSalvos"))
+    if (!dataSalvos) {
+        dataSalvos = dbCursosSalvos.salvos
+    }
+    for (i=0; i < dataSalvos.length ; i++) {
+            let curso = dataSalvos[i]
+            htmlStr +=
+            `<li class="container-curso">
+                <a href="curso.html?idCurso=${curso.idCurso}&id=0" class="col-lg-3">
+                    <img src="${curso.img}" alt="">
+                </a>
+                <div class="container-texto">
+                    <button type="button" onclick="removeCurso(${i})"><ion-icon name="heart" class="btn-coracao"></ion-icon></button> 
+                    <h5>${curso.nomeCurso}</h5>
+                    <p class="disciplina"><span>Disciplina:</span> ${db.disciplinas[curso.idDisciplina].titulo}</p>
+                    <p class="disciplina"><span>Descrição:</span> ${curso.descricao}</p>
+                </div>
+            </li>`
+    }
+    containerCurso.innerHTML = htmlStr
+}
+// remover curso selecionado pelo usuario
+function removeCurso(index) {
+    dataSalvos = JSON.parse(localStorage.getItem("cursosSalvos"))
+
+    dataSalvos.splice(index,1);
+    localStorage.setItem("cursosSalvos", JSON.stringify(dataSalvos))
+
+    carregaCursosSalvos ()
+}
