4983fca453e57b83f66028eff48835f4b2ab3b6e
fernandamgomes <fernanda.gomes.1117337@sga.pucminas.br>
2021-12-08 13:09:47 -0300

    Update disciplinas.js

--- Codigo/sprint04/scripts/disciplinas.js
+++ Codigo/sprint04/scripts/disciplinas.js
@@ -1,13 +1,14 @@
 // carrega o menu de disciplinas
-function carregaDisciplinas () { 
-    let menuDisciplinas = document.getElementById ('menu_disciplinas')
+function carregaDisciplinas() {
+    let menuDisciplinas = document.getElementById('menu_disciplinas')
     let htmlStr = ''
-    for (i=0; i < db.disciplinas.length; i++) {
+    for (i = 0; i < db.disciplinas.length; i++) {
         let disciplina = db.disciplinas[i]
         htmlStr += `<li><a class="dropdown-item" href="disciplina.html?id=${disciplina.id}">${disciplina.titulo}</a></li>`
     }
     menuDisciplinas.innerHTML = htmlStr
 }
+
 
 function getUrlVars() {
     var vars = {};
@@ -19,23 +20,25 @@ function getUrlVars() {
     return vars;
 }
 
+
 // carrega a listra de sites indicados por disciplinas
-function carregaListaSites () {
+function carregaListaSites() {
     let listaSites = document.getElementById('lista-sites')
     let id = getUrlVars()["id"]
     let sites = db.disciplinas[id].site
     let htmlStr = ''
 
-    for (i=0; i < sites.length; i++) {
+    for (i = 0; i < sites.length; i++) {
         let site = sites[i]
         htmlStr += `<li><a href="${site.url}" target="_blank" class="btn">${site.nome}</a></li>`
     }
     listaSites.innerHTML = htmlStr
 }
 
+
 // carrega o titulo da pagina (nome da disciplina)
-function carregaTitulo () {
-    let titulo = document.getElementById ('titulo')
+function carregaTitulo() {
+    let titulo = document.getElementById('titulo')
     let id = getUrlVars()["id"]
     let titulo_pag = db.disciplinas[id].titulo
     let htmlStr = ''
@@ -46,16 +49,17 @@ function carregaTitulo () {
     document.title = titulo_pag
 }
 
+
 // carrega a lista de cursos cadastrados por disciplina
-function carregaCursos () {
+function carregaCursos() {
     let containerCurso = document.getElementById('lista_cursos')
     let id = getUrlVars()["id"]
     let htmlStr = ''
-    for (i=0; i < db.cursos.length; i++) {
+    for (i = 0; i < db.cursos.length; i++) {
         if (db.cursos[i].idDisciplina == id) {
             let curso = db.cursos[i]
             htmlStr +=
-            `<li class="container-curso">
+                `<li class="container-curso">
                 <a href="curso.html?idCurso=${curso.idCurso}&id=0">
                 <img src="${curso.img}" alt="">
                 <div class="container-texto">
@@ -70,11 +74,12 @@ function carregaCursos () {
     containerCurso.innerHTML = htmlStr
 }
 
+
 // carrega o menu na pagina inicial com as disciplinas e os respectvos icones
-function carregaDisciplinasIndex () {
+function carregaDisciplinasIndex() {
     let menuIndex = document.getElementById('menuIndex')
     let htmlStr = ''
-    for (i=0; i < db.disciplinas.length; i++) {
+    for (i = 0; i < db.disciplinas.length; i++) {
         let disciplina = db.disciplinas[i]
         htmlStr +=
             `<li class="col-6 col-sm-6 col-md-3 col-lg-3 container-mat">
