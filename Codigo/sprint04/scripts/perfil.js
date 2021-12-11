ff0353a5cf7d142f10017e07ff911f8a39554673
fernandamgomes <fernanda.gomes.1117337@sga.pucminas.br>
2021-12-11 15:06:19 -0300

    edicao de perfil - apagar perfil

--- Codigo/sprint04/scripts/perfil.js
+++ Codigo/sprint04/scripts/perfil.js
@@ -1,3 +1,47 @@
 function preencheForm() {
     let usuario = JSON.parse(sessionStorage.getItem('usuarioLogado'));
+    let form = document.getElementById('form-componentes')
+    let htmlStr = `<input type="text" class="form-control" id="inputId" hidden disabled>
+                        
+                    <label for="inputNome" class="form-label">Nome Completo:</label>
+                    <input type="text" class="form-control" id="inputNome" value="${usuario.nome}">
+
+                    <label for="inputUsuario" class="form-label">Nome de Usuário</label>
+                    <input type="text" class="form-control" id="inputUsuario" value="${usuario.usuario}">
+
+                    <label for="inputEmail" class="form-label">Endereço de Email</label>
+                    <input type="email" class="form-control" id="inputEmail" value="${usuario.email}">
+
+                    <label for="inputPassword" class="form-label">Digite uma senha</label>
+                    <input type="password" class="form-control" id="inputSenha">
+
+                    <label for="inputPassword" class="form-label">Confirme sua senha</label>
+                    <input type="password" class="form-control" id="inputConfirmSenha">`
+
+    form.innerHTML = htmlStr
 }
+
+
+
+function apagarPerfil() {
+    alert("oi")
+    let usuario = JSON.parse(sessionStorage.getItem('usuarioLogado'));
+    let id = usuario.id
+    let dbCadastro = JSON.parse(localStorage.getItem('db_cadastro'));
+
+    dbCadastro = dbCadastro.filter(function (element) { return element.id != id });
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_cadastro', JSON.stringify(dbCadastro));
+
+    //Atualiza dados no session storage
+    sessionStorage.removeItem ('usuarioLogado');
+    window.location.href = 'index.html';
+}
+
+function salvarAlteracoes() {
+    
+    
+}
+
+
+
