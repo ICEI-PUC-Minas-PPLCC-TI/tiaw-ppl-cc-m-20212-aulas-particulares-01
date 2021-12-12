7e8795948bc34d6247057cbfe51effed5ef9a560
fernandamgomes <fernanda.gomes.1117337@sga.pucminas.br>
2021-12-12 13:06:02 -0300

    Update salvarCurso.js

--- Codigo/sprint04/scripts/salvarCurso.js
+++ Codigo/sprint04/scripts/salvarCurso.js
@@ -22,62 +22,78 @@ function salvarCurso(idCurso) {
     // pagina so e acessada se o usuario estiver logado
     // puxar dados do usuario para  salvar o curso associado ao id do usuario
     let usuario = JSON.parse(sessionStorage.getItem('usuarioLogado'));
-
-    dataSalvos = JSON.parse(localStorage.getItem("cursosSalvos"))
-    if (!dataSalvos) {
-        dataSalvos = dbCursosSalvos.salvos
-    }
-    // se o curso ja tiver sido salvo pelo usuario teste = 1
-    for (i = 0; i < dataSalvos.length; i++) {
-        if ((dataSalvos[i].idCurso == idCurso) && (usuario.id === dataSalvos[i].idUsuario)) {
-            teste = 1;
-        }
-    }
-    // se o curso nao tiver sido salvado pelo usuario
-    if (teste == 0) {
+    if (Object.keys(usuario).length === 0) {
         htmlStr = 
-            `<div class="modal-dialog modal-dialog-centered modal-sm">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                `<div class="modal-dialog modal-dialog-centered modal-md">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h6>faça login para conseguir salvar esse curso!</h6>
+                        </div>
+                        <div class="modal-footer">
+                        </div>
                     </div>
-                    <div class="modal-body">
-                        <h6>curso salvado com sucesso!</h6>
-                    </div>
-                    <div class="modal-footer">
-                    </div>
-                </div>
-            </div>`
-        let curso = db.cursos[idCurso]
-        tmp = {
-            "idDisciplina": curso.idDisciplina,
-            "idCurso": idCurso,
-            "nomeCurso": curso.nomeCurso,
-            "descricao": curso.descricao,
-            "img": curso.img,
-            "idUsuario": usuario.id,
-        }
-        dataSalvos.push(tmp);
+                </div>`
     }
-    // se ja tiver sido salvado, nao salvar mais uma vez
     else {
-        htmlStr = 
-            `<div class="modal-dialog modal-dialog-centered modal-sm">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        dataSalvos = JSON.parse(localStorage.getItem("cursosSalvos"))
+        if (!dataSalvos) {
+            dataSalvos = dbCursosSalvos.salvos
+        }
+        // se o curso ja tiver sido salvo pelo usuario teste = 1
+        for (i = 0; i < dataSalvos.length; i++) {
+            if ((dataSalvos[i].idCurso == idCurso) && (usuario.id === dataSalvos[i].idUsuario)) {
+                teste = 1;
+            }
+        }
+        // se o curso nao tiver sido salvado pelo usuario
+        if (teste == 0) {
+            htmlStr = 
+                `<div class="modal-dialog modal-dialog-centered modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h6>curso salvado com sucesso!</h6>
+                        </div>
+                        <div class="modal-footer">
+                        </div>
                     </div>
-                    <div class="modal-body">
-                        <h6>esse curso já estava salvo!</h6>
+                </div>`
+            let curso = db.cursos[idCurso]
+            tmp = {
+                "idDisciplina": curso.idDisciplina,
+                "idCurso": idCurso,
+                "nomeCurso": curso.nomeCurso,
+                "descricao": curso.descricao,
+                "img": curso.img,
+                "idUsuario": usuario.id,
+            }
+            dataSalvos.push(tmp);
+        }
+        // se ja tiver sido salvado, nao salvar mais uma vez
+        else {
+            htmlStr = 
+                `<div class="modal-dialog modal-dialog-centered modal-sm">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <h6>esse curso já estava salvo!</h6>
+                        </div>
+                        <div class="modal-footer">
+                        </div>
                     </div>
-                    <div class="modal-footer">
-                    </div>
-                </div>
-            </div>`
+                </div>`
+        }
+        localStorage.setItem("cursosSalvos", JSON.stringify(dataSalvos));
     }
+    
     modal.innerHTML = htmlStr;
-
-    localStorage.setItem("cursosSalvos", JSON.stringify(dataSalvos));
 }
 
 function btnclick() {
